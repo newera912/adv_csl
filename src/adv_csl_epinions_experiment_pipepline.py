bf27728f2a17dd0bb79bbae031a879820a4c8c00
Adilijiang Alimu <alex_912@live.cn>
2019-01-05 09:43:35 -0500

    Jan5

--- src/adv_csl_epinions_experiment_pipepline.py
+++ src/adv_csl_epinions_experiment_pipepline.py
@@ -14,6 +14,7 @@ from network_funs import *
 from feng_SL_inference_multiCore import *
 
 from multi_core_csl_inference_adversarial_epinions import inference_apdm_format as inference_apdm_format_conflict_evidence
+from multi_core_csl_inference_no_adversarial_epinions import inference_apdm_format as inference_NoAdvTraining
 
 #from SL_inference import *
 from random import shuffle
@@ -68,27 +69,27 @@ def main():
 def experiment_proc_server():
     logging = Log()
     data_root = "/network/rit/lab/ceashpc/adil/data/adv_csl/Jan2/"  #May23 May23-3
-    methods = ["CSL", "Adv-CSL", "SL"][1:2]
+    methods = ["CSL", "Adv-CSL", "SL","NAT-CSL"][1:2]
     # graph_sizes = [500, 1000, 5000, 10000, 47676]
     graph_sizes = [1000, 5000,10000,47676]
     ratios = [0.1, 0.2, 0.3,0.4,0.5,0.6,0.7,0.8]
     realizations = 10
     case_count=0
-    for adv_type in ["random_flip","random_noise","random_pgd2"][:2]:
+    for adv_type in ["random_flip","random_noise","random_pgd"][2:]:
         for graph_size in graph_sizes[1:2]:
             for T in [8, 9, 10, 11][2:3]:    #5,6,10,20,11,21,15
                 for real_i in range(realizations)[:1]:
                     for ratio in [0.2][:]:#0.0,0.1,0.2,0.3,the percentage of edges set the observations to 1
                         for swap_ratio in [0.00, 0.01, 0.05][:1]:
-                            for test_ratio in [0.1, 0.2,0.3, 0.4, 0.5][:]:                #percentage of edges to test (|E_x|/|E|)
-                                for gamma in [0.0, 0.01, 0.03, 0.05, 0.07,0.09,0.11,0.13,0.15,0.20,0.25][9:]:  # 8
+                            for test_ratio in [0.1, 0.2,0.3, 0.4, 0.5][4:]:                #percentage of edges to test (|E_x|/|E|)
+                                for gamma in [0.0, 0.01, 0.03, 0.05, 0.07,0.09,0.11,0.13,0.15,0.20,0.25][:]:  # 8
                                     out_folder = data_root +"/"+adv_type +"/"+ str(graph_size) + "/"
                                     logging.write(str(case_count)+" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
                                     case_count += 1.0
                                     for method in methods[:]:
                                         f = out_folder + "nodes-{}-T-{}-rate-{}-testratio-{}-swaprate-{}-gamma-{}-realization-{}-data-X.pkl".format(
                                             graph_size, T, ratio, test_ratio, swap_ratio, gamma, real_i)
-                                        outf = '../output/epinions/{}_results-server-{}-Jan3-{}-debug.json'.format(method,graph_size,adv_type)
+                                        outf = '../output/epinions/{}_results-server-{}-Jan3-{}-2.json'.format(method,graph_size,adv_type)
 
                                         logging.write("method: {}, T, {}, real_i: {}, ratio: {}, test_ratio: {}, swaprate: {},gamma:{}, graph_size: {}".format(method, T, real_i, ratio, test_ratio, swap_ratio,gamma, graph_size))
                                         logging.write(f)
@@ -181,13 +182,13 @@ def evaluate(V, E, Obs, Omega, E_X, X_b, logging, method = 'csl', psl = False, a
         # print pred_omega_x
     elif method == 'CSL':
         pred_omega_x = inference_apdm_format(V, E, Obs, Omega, E_X, logging)
-    elif method == 'CI1':
+    elif method == 'NAT-CSL':
         # b = {e: 0 for e in E}
-        b={}
-        # X_b = []
+        b = {}
+        # X_b = {e: 0 for e in E if e not in E_X}
         X_b = {e: 0 for e in E if not E_X.has_key(e)}
-        psl = True
-        pred_omega_x, _ = inference_apdm_format_conflict_evidence(V, E, Obs, Omega, b, X_b, E_X, logging, psl)
+        psl = False
+        pred_omega_x, _ = inference_NoAdvTraining(V, E, Obs, Omega, b, X_b, E_X, logging, psl)
     elif method == 'Adv-CSL':
         # b = {e: 0 for e in E}
         b={}
